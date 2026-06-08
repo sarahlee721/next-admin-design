31f7d961507d5c04c337591e6c52ed3b135c9f25
Sara Lee <leesarah@puritechlab.com>
2026-06-08 14:47:34 +0900

    수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/next-admin-design',
+  assetPrefix: '/next-admin-design/',
 };
 
 export default nextConfig;
